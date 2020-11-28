44c13910dc4b0f85fc77182722b43ea932a5c57e
Maria Elena Tripoli <meatripoli@gmail.com>
2020-11-28 09:11:50 -0600

    working on google oath

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../styles/App.css';
-
+import {Helmet} from "react-helmet";
 
 function App(props) {
   return (<>
-    {/* <Helmet>
-      <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com"></meta>
-    </Helmet> */}
+    <Helmet>
+      <meta name="google-site-verification" content="foj5RCU5Qt108gh3kdWrRPBkgD_xDA48xoqeKqtinMQ" />
+    </Helmet>
     <div className="App">
       <header className="App-header">
         <h1>{props.name}</h1>
+        <div class="g-signin2" data-onsuccess="onSignIn">hello</div>
       </header>
       <body className="App-body">  
+        
         <script src="https://apis.google.com/js/platform.js" async defer></script>    
       </body>
     </div>
